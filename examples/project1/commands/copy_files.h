757a69e88526ea1547f69ded261f72e3a71e3b8e
Eden Reich <eden.reich@gmail.com>
2019-06-08 21:24:32 +0200

    add @name notations

--- examples/project1/commands/copy_files.h
+++ examples/project1/commands/copy_files.h
@@ -4,6 +4,7 @@
 #include <console/types/exit_code.h>
 
 /**
+ * @name copy-files
  * @description copy files from <source> to <dist>
  */
 class CopyFiles : public Command
--- examples/project1/commands/hello_world.h
+++ examples/project1/commands/hello_world.h
@@ -4,6 +4,7 @@
 #include <console/types/exit_code.h>
 
 /**
+ * @name hello-world
  * @description output hell world to the console
  */
 class HelloWorld : public Command
