1cdde4d4c2d1f96635999fa68c0f6e78b5cd2ebf
Eden Reich <eden.reich@gmail.com>
2019-06-08 19:29:10 +0200

    update example project

--- examples/project1/main.cpp
+++ examples/project1/main.cpp
@@ -7,14 +7,15 @@ int main(int argc, char * argv[])
     Application app(argc, argv);
 
     app.setApplicationName("Todo List Application");
+    app.setApplicationUsage("todo [command] [options]");
     app.setApplicationVersion("1.0");
 
     app.setApplicationDescription("Todo List Application");
 
+    app.setCommandsDirectoryPath("src/commands");
+
     app.addCommand(new CopyFiles);
     app.addCommand(new HelloWorld);
-
-    app.printHelp();
 
     return app.run();
 }
