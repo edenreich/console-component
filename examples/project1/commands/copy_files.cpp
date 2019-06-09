0d48ad37be8018f45327c530099a2b51ff3492d7
Eden Reich <eden.reich@gmail.com>
2019-06-09 02:23:36 +0200

    add examples

--- examples/project1/commands/copy_files.cpp
+++ examples/project1/commands/copy_files.cpp
@@ -4,7 +4,14 @@
 
 ExitCode CopyFiles::handle(std::vector<std::string> options)
 {
-    std::cout << "command copy files was called";
+    std::cout << "command copy files was called\n" << std::endl;
+
+    std::cout << "with options: " << std::endl;
+    
+    for (auto & option : options) 
+    {
+        std::cout << option << std::endl;
+    }
 
     return ExitCode::Ok;
 }
--- examples/project1/commands/hello_world.cpp
+++ examples/project1/commands/hello_world.cpp
@@ -4,7 +4,14 @@
 
 ExitCode HelloWorld::handle(std::vector<std::string> options)
 {
-    std::cout << "command hello world was called";
+    std::cout << "command hello world was called" << std::endl;
+
+    std::cout << "with options: " << std::endl;
+    
+    for (auto & option : options) 
+    {
+        std::cout << option << std::endl;
+    }
 
     return ExitCode::Ok;
 }
