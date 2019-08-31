f6dddd7aad8f4637e247b8f814253d1a210dc56a
Eden Reich <eden.reich@gmail.com>
2019-08-31 03:49:26 +0200

    organize tests a bit

---
+++ tests/unit/output_test.cpp
@@ -0,0 +1,11 @@
+
+#include <gtest/gtest.h>
+#include <iostream>
+#include <console/application.h>
+#include <console/output.h>
+#include <console/types/colors.h>
+
+
+TEST(OutputInterfaceTest, ItOutputsFormattedMessages) {
+    // not yet implemented, need somehow to extract the color output to another method
+}
