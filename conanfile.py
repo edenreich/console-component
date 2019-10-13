9c15134daca57e2993ef236f339539da2ae36f36
Eden Reich <eden.reich@gmail.com>
2019-10-13 16:41:22 +0200

    package pre-built binaries to package-registry

---
+++ conanfile.py
@@ -0,0 +1,39 @@
+from conans import ConanFile, CMake
+from conans.tools import download, unzip
+import os
+
+
+class ConsolePackage(ConanFile):
+    name = "console-component"
+    version = "v1.0.2"
+    author = "Eden Reich <eden.reich@gmail.com>"
+    url = "https://github.com/edenreich/console-component"
+    description = "An easy to use component for building powerful console applications written in C++"
+    topics = (
+        "cpp", "cpp14", "console", 
+        "console-component", "cli", 
+        "command-line", "commandline", 
+        "cmake", "cross-platform", 
+        "cpp-component", "cpp17"
+    )
+    homepage = "https://github.com/edenreich/console-component"
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake"
+    cmake = None
+    options = {"shared": [True, False]}
+    default_options = {"shared": False}
+
+    def source(self):
+        zip_binaries = "binaries_{}.zip".format(self.version)
+        zip_includes = "includes_{}.zip".format(self.version)
+        download("https://github.com/edenreich/console-component/releases/download/{}/binaries.zip".format(self.version), zip_binaries, retry=3, retry_wait=5)
+        download("https://github.com/edenreich/console-component/releases/download/{}/includes.zip".format(self.version), zip_includes, retry=3, retry_wait=5)
+        unzip(zip_binaries, "dist/lib")
+        unzip(zip_includes, "dist/include")
+        os.unlink(zip_binaries)
+        os.unlink(zip_includes)
+
+    def package(self):
+        self.source()
+        self.copy("*", dst="include", src="dist/include")
+        self.copy("*", dst="lib", src="dist/lib")
