bb911901cab81c2a4d2ddd6c94930c322d39cc19
Eden Reich <eden.reich@gmail.com>
2020-08-02 23:49:18 +0200

    fix formating

--- src/application.cpp
+++ src/application.cpp
@@ -32,7 +32,7 @@ Application::Application(int& argc, char** argv)
  */
 Application::~Application()
 {
-    for (auto& commandNamespace: getAvailableCommands())
+    for (auto& commandNamespace : getAvailableCommands())
     {
         for (auto& command : commandNamespace.second)
         {
